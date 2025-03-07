6338493df76c4f58e6e1db6c970f5bdf2a4338f8
boxer <andreykakornilov2006@mail.ru>
2025-03-07 16:36:45 +0300

    8 commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
+import * as React from 'react';
+import { cn } from '@/lib/utils';
+// import { Container } from './shared/container';
 
 
-
-
+interface Props {  
+  className?: string;  
+}  
+   
 export default function Home() {
   return (
-  <>
-   <p className='text-left px-20 py-10 text-4xl font-black'>Все пиццы</p>
-  </>
-
-   );
-}
-
+    <header className={cn('border border-b')}>
+      {/* <Container className='flex items-center justify-between py-8'> */}
+          <div className='flex gap-4'>
+          <p className='text-2xl font-black text-base text-base text-gray-400'>Все питццы</p>
+          </div>    
+      {/* </Container> */}
+    </header>    
+   ); 
+}    
