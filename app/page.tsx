61929a15485a30ff737e2a447fcc04d26b852f2b
boxer <andreykakornilov2006@mail.ru>
2025-03-13 12:21:37 +0300

    9 commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,26 @@
 import * as React from 'react';
 import { cn } from '@/lib/utils';
-// import { Container } from './shared/container';
+// import { Container } from "components/ui/button.tsx";
+// import { Button } from '../ui/button';
+import { ShoppingCart, User, UserRound } from 'lucide-react';
+
 
 
 interface Props {  
-  className?: string;  
+  className?: string;      
 }  
-   
-export default function Home() {
+ 
+
+export const Home: React.FC<Props> = ({ className }) => {
   return (
     <header className={cn('border border-b')}>
       {/* <Container className='flex items-center justify-between py-8'> */}
           <div className='flex gap-4'>
-          <p className='text-2xl font-black text-base text-base text-gray-400'>Все питццы</p>
+          <p className='text-4xl font-black px-20 py-10'>Все питццы</p>
+          {/* <Button variant='outline' className='gap-1 flex items-center'><UserRound size={13}/>Всё</Button> */}
           </div>    
       {/* </Container> */}
     </header>    
    ); 
 }    
+  
